4fe1018eed4fe0305ae18bf4a701012458c37a10
UserName504 <77865535+UserName504@users.noreply.github.com>
2021-03-12 15:53:36 +0000

    Update Fire.m

--- Fire.m
+++ Fire.m
@@ -1,28 +1,20 @@
-%**************************************************************************
-% Y3854349
-% University of York
-% Department of Electronic Engineering
-% MSc Audio and Music Technology
-% ELE00087M: Audio Signals and Psychoacoustics
-%**************************************************************************
-% Implementation of convolution through the overlap-add method, with a
-% fixed impulse response.
-%**************************************************************************
-%
+% LOAD HRIR dataset here:
 load HRIRs_0el_IRC_subject59
 
-[fire, wav_Fs] = audioread('Fire.wav'); % Load audio file.
+% LOAD audio input file here:
+[fire, wav_Fs] = audioread('Fire.wav');
 if wav_Fs ~= Fs; 
     error('Sampling frequencies must be the same'); 
 end
-fireR    = fire(:, 1);       % (If necessary) this reduces a stereo input to mono
-fireL    = fire(:, 1);
-Ninput   = length(fire);     % The number of samples in the input signal
-y_length = Ninput + 512 - 1; % The number of samples created by convolving x and IR
-Noutput  = y_length;         % and therefore the number of output samples
 
-frame_size     = 1024;                          % The number of samples in a frame
-frame_conv_len = 1024 + 512 - 1;                % The number of samples created by convolving a frame of x and IR
+fireR    = fire(:, 1);          % This reduces a stereo input file to MONO, if necessary
+fireL    = fire(:, 1);          % This reduces a stereo input file to MONO, if necessary
+Ninput   = length(fire);        % Get the number of samples in the input signal, x
+y_length = Ninput + 512 - 1;    % Number of samples created by convolving the input, x, with the impulse response
+Noutput  = y_length;            % The number of output samples following convolution
+
+frame_size     = 1024;                          % Number of samples per frame
+frame_conv_len = 1024 + 512 - 1;                % Number of samples resulting from convolving a frame of x with with the impulse response
 step_size      = 512;                           % Step size for 50% overlap-add
 w              = hann(frame_size, 'periodic');  % Generate the Hann function to window a frame
 Nframes        = floor(Ninput / step_size) - 1; % -1 prevents input overrun in the final frame
@@ -50,9 +42,10 @@ for n = 1 : Nframes
     % Advance to the start of the next frame
     frame_start = step_size + frame_start;
 end
+
 display(['Computation took ' num2str(toc) ' seconds'])
 y(:, 1) = yL;
 y(:, 2) = yR;
 
-% Save spatialised sound as a new audio file.
+% Save spatialised sound as a new audio file:
 audiowrite('Fire_Spatialised.wav',fire, wav_Fs); 
