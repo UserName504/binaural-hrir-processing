c46d4e9b8bdafc2e13a5b18e70aa602d2d99bf61
UserName504 <77865535+UserName504@users.noreply.github.com>
2021-03-12 15:42:31 +0000

    Update DoorOpening.m

--- DoorOpening.m
+++ DoorOpening.m
@@ -1,28 +1,20 @@
-%**************************************************************************
-% Y3854349
-% University of York
-% Department of Electronic Engineering
-% MSc Audio and Music Technology
-% ELE00087M: Audio Signals and Psychoacoustics
-%**************************************************************************
-% Implementation of convolution through the overlap-add method, with a
-% fixed impulse response.
-%**************************************************************************
-%
+% LOAD HRIR dataset here:
 load HRIRs_0el_IRC_subject59
 
+% LOAD audio input file here:
 [open, wav_Fs] = audioread('DoorOpen.wav'); % Load audio file.
 if wav_Fs ~= Fs; 
     error('Sampling frequencies must be the same'); 
 end
-openR    = open(:, 1);       % Split STEREO to MONO, if necessary.
-openL    = open(:, 1);
-Ninput   = length(open);     % The number of samples in the input signal
-y_length = Ninput + 512 - 1; % The number of samples created by convolving x and IR
-Noutput  = y_length;         % and therefore the number of output samples
 
-frame_size     = 1024;                          % The number of samples in a frame
-frame_conv_len = 1024 + 512 - 1;                % The number of samples created by convolving a frame of x and IR
+openR    = open(:, 1);          % This reduces a stereo input file to MONO, if necessary
+openL    = open(:, 1);          % This reduces a stereo input file to MONO, if necessary
+Ninput   = length(open);        % Get the number of samples in the input signal, x
+y_length = Ninput + 512 - 1;    % Number of samples created by convolving the input, x, with the impulse response
+Noutput  = y_length;            % The number of output samples following convolution
+
+frame_size     = 1024;                          % Number of samples per frame
+frame_conv_len = 1024 + 512 - 1;                % Number of samples resulting from convolving a frame of x with with the impulse response
 step_size      = 512;                           % Step size for 50% overlap-add
 w              = hann(frame_size, 'periodic');  % Generate the Hann function to window a frame
 Nframes        = floor(Ninput / step_size) - 1; % -1 prevents input overrun in the final frame
