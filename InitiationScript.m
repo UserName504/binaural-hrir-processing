dc01b4ddcc6927eb80bbf0e0fcc70d5634efbc19
UserName504 <77865535+UserName504@users.noreply.github.com>
2021-03-12 15:43:14 +0000

    Update InitiationScript.m

--- InitiationScript.m
+++ InitiationScript.m
@@ -1,23 +1,24 @@
+% LOAD HRIR dataset here:
 load HRIRs_0el_IRC_subject59
 
 open  = audioread('DoorOpening.wav');
-shut  = audioread('DoorClosed.wav' );
-alarm = audioread('Alarm.wav'      );
-fire  = audioread('Fire.wav'       );
-walk  = audioread('Walking.wav'    );
+shut  = audioread('DoorClosed.wav');
+alarm = audioread('Alarm.wav');
+fire  = audioread('Fire.wav');
+walk  = audioread('Walking.wav');
 N     = 1400000;
-open  = DoorOpen (open );
-shut  = DoorClose(shut );
+open  = DoorOpen (open);
+shut  = DoorClose(shut);
 alarm = AlarmRing(alarm);
-fire  = FireBurn (fire );
-walk  = WalkPan  (walk );
+fire  = FireBurn (fire);
+walk  = WalkPan  (walk);
 
-% Zero Padding:
-open    = vertcat(open,  zeros(N - length(open ), 2));
-walk    = vertcat(walk,  zeros(N - length(walk ), 2));
+% Zero padding, to ensure that all input signal are the same length:
+open    = vertcat(open, zeros(N - length(open), 2));
+walk    = vertcat(walk, zeros(N - length(walk), 2));
 alarm   = vertcat(alarm, zeros(N - length(alarm), 2));
-fire    = vertcat(fire,  zeros(N - length(fire ), 2));
-shut    = vertcat(shut,  zeros(N - length(shut ), 2));
+fire    = vertcat(fire, zeros(N - length(fire), 2));
+shut    = vertcat(shut, zeros(N - length(shut), 2));
 MixDown = open + shut + alarm + fire + walk;
 
 normalizedOutput = zeros(N,2);
@@ -26,4 +27,4 @@ maxVolume        = max(max(abs(MixDown)));
 normalizedOutput = (MixDown/maxVolume) * 0.99;
 
 sound(normalizedOutput,44100)
-audiowrite('Y3854349_ELE00087M.wav',normalizedOutput, Fs);
+audiowrite('BinauralOutput.wav',normalizedOutput, Fs);
